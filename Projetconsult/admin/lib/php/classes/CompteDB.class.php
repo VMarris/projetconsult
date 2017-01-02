2f603b7f2cbe2ac71ce983accb9a3f75a6af5d8e
Marris <v.m.74@hotmail.com>
2017-01-02 19:24:31 +0100

    Fin inscription
    l'inscription est fonctionnelle

--- Projetconsult/admin/lib/php/classes/CompteDB.class.php
+++ Projetconsult/admin/lib/php/classes/CompteDB.class.php
@@ -22,7 +22,22 @@ class CompteDB extends Compte {
         }
         return $retour;
     }
-    
+
+    function inscription($mail, $mdp, $nom, $prenom, $phone) {
+        try {
+            $querry = "select inscription(:mail,md5(:mdp),:nom,:prenom,:phone)";
+            $sql = $this->_db->prepare($querry);
+            $sql->bindValue(1, $mail);
+            $sql->bindValue(2, $mdp);
+            $sql->bindValue(3, $nom);
+            $sql->bindValue(4, $prenom);
+            $sql->bindValue(5, $phone);
+            $sql->execute();
+        } catch (PDOException $e) {
+            print $e->getMessage();
+        }
+    }
+
     function exist($email) {
         $retour = array();
         try {
@@ -36,5 +51,5 @@ class CompteDB extends Compte {
         }
         return $retour;
     }
- 
+
 }
--- Projetconsult/pages/inscription.php
+++ Projetconsult/pages/inscription.php
@@ -1,111 +1,116 @@
-<div class="centrer">
-    <h2 class="textnorm dblue">Inscription</h2>
-</div>
+
 <?php
 //traitement php formulaire
-$erreur="";
+$erreur = "";
 if (isset($_POST['commander'])) {
     extract($_POST, EXTR_OVERWRITE);
 
     if (empty($email1) || empty($email2) || empty($nom) || empty($prenom) || empty($mdp1) || empty($mdp2)) {
         $erreur = "- Veuillez renseigner tous les champs";
     } else {
-        $test=true;
+        $test = true;
         //vérific champ téléphone
-        if(!empty($telephone) and preg_match("#[0-9]{3}\/[0-9]{2}\.[0-9]{2}\.[0-9]{2}#", $telephone) == false){
-            $erreur = $erreur."- Entrez un numero de téléphone valide<br/>";
-            $test=false;
+        if (!empty($telephone) and preg_match("#[0-9]{3}\/[0-9]{2}\.[0-9]{2}\.[0-9]{2}#", $telephone) == false) {
+            $erreur = $erreur . "- Entrez un numero de téléphone valide<br/>";
+            $test = false;
         }
-        if($email1 != $email2){
-            $erreur = $erreur."- Entrez deux e-mails identiques<br/>";
-            $test=false;
+        if ($email1 != $email2) {
+            $erreur = $erreur . "- Entrez deux e-mails identiques<br/>";
+            $test = false;
         }
-        if($mdp1 != $mdp2){
-            $erreur = $erreur."- Entrez deux mot de passe identiques<br/>";
-            $test=false;
-        }
-        
-        if($test){
-            $log = new CompteDB($cnx);
-            $retour = $log->exist($email1);
-            if($retour=="0"){
-                
-            }else{
-                $erreur = $erreur."- Email déjà utilisé<br/>";
-            }
+        if ($mdp1 != $mdp2) {
+            $erreur = $erreur . "- Entrez deux mot de passe identiques<br/>";
+            $test = false;
         }
 
+        if ($test) {
+            $log = new CompteDB($cnx);
+            $retour = $log->exist($email1);
+            $okay = "Inscription effectuée";
+            if ($retour == "0") {
+                $log->inscription($email1, $mdp1, $nom, $prenom, $telephone);
+            } else {
+                $erreur = $erreur . "- Email déjà utilisé<br/>";
+            }
+        }
     }
 }
 ?>
-    <div id="div_form_inscript">
-        Veuillez entrer vos coordonnées : <br/><br/>
+<div class="centrer">
+    <h2 class="textnorm dblue">Inscription</h2>
+    <?php
+    if (!empty($erreur))
+        print "<h2 class='green'>" . $okay . "</h2>";
+    ?>
+</div>
+<div id="div_form_inscript">
+    Veuillez entrer vos coordonnées : <br/><br/>
 
-        <div class="container ">
-            <div class="row">
-                <div class="col-sm-4 txtGras txtRouge" id="diverins"> 
+    <div class="container ">
+        <div class="row">
+            <div class="col-sm-4 txtGras txtRouge" id="diverins"> 
                 <?php
                 if (!empty($erreur))
-                    print "<span>".$erreur."</span>";
+                    print "<span>" . $erreur . "</span>";
                 ?>
-                </div>
-                <br/>
             </div>
-            <form action="<?php print $_SERVER['PHP_SELF']; ?>" method="post" id="form_inscription">
-
-                <div class="row">
-                    <div class="col-sm-2"><label for="email1">Email</label></div>
-                    <div class="col-sm-4">
-                        <input type="email" id="email1" name="email1" placeholder="aaa@aaa.aa"/>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-sm-2"><label for="email2" >Confirmez votre email</label></div>
-                    <div class="col-sm-4">
-                        <input type="email" id="email2" name="email2" placeholder="aaa@aaa.aa"/>
-                    </div>
-                </div> 
-                <div class="row">
-                    <div class="col-sm-2"><label for="mdp1">Mot de passe</label></div>
-                    <div class="col-sm-4">
-                        <input type="password" id="mdp1" name="mdp1"/>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-sm-2"><label for="mdp12">Confirmez votre mot de passe</label></div>
-                    <div class="col-sm-4">
-                        <input type="password" id="mdp2" name="mdp2"/>
-                    </div>
-                </div> 
-                <div class="row">
-                    <div class="col-sm-2"><label for="nom">Nom</label></div>
-                    <div class="col-sm-4">
-                        <input type="text" name="nom" id="nom" />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-2"><label for="prenom">Prénom</label></div>
-                    <div class="col-sm-4">
-                        <input type="text" name="prenom" id="prenom" />
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-sm-2"><label for="telephone">Téléphone</label></div>
-                    <div class="col-sm-4">
-                        <input type="text" name="telephone" id="telephone" placeholder="xxx/xx.xx.xx"/>
-                    </div>
-                </div>
-                <br/>
-                <div class="row">
-                    <div class="col-sm-2 unpeuplace">
-                        <input type="reset" id="reset" value="Annuler" />
-                    </div>
-                    <div class="col-sm-4 unpeuplace">
-                        <input type="submit" name="commander" id="inscrire" value="Finaliser mon inscription" />         
-                    </div>
-                </div>
-            </form>
+            <br/>
         </div>
+        <form action="<?php print $_SERVER['PHP_SELF']; ?>" method="post" id="form_inscription">
+
+            <div class="row">
+                <div class="col-sm-2"><label for="email1">Email</label></div>
+                <div class="col-sm-4">
+                    <input type="email" id="email1" name="email1" placeholder="aaa@aaa.aa"/>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-2"><label for="email2" >Confirmez votre email</label></div>
+                <div class="col-sm-4">
+                    <input type="email" id="email2" name="email2" placeholder="aaa@aaa.aa"/>
+                </div>
+            </div> 
+            <div class="row">
+                <div class="col-sm-2"><label for="mdp1">Mot de passe</label></div>
+                <div class="col-sm-4">
+                    <input type="password" id="mdp1" name="mdp1"/>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-2"><label for="mdp12">Confirmez votre mot de passe</label></div>
+                <div class="col-sm-4">
+                    <input type="password" id="mdp2" name="mdp2"/>
+                </div>
+            </div> 
+            <div class="row">
+                <div class="col-sm-2"><label for="nom">Nom</label></div>
+                <div class="col-sm-4">
+                    <input type="text" name="nom" id="nom" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-2"><label for="prenom">Prénom</label></div>
+                <div class="col-sm-4">
+                    <input type="text" name="prenom" id="prenom" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-2"><label for="telephone">Téléphone</label></div>
+                <div class="col-sm-4">
+                    <input type="text" name="telephone" id="telephone" placeholder="xxx/xx.xx.xx"/>
+                </div>
+            </div>
+            <br/>
+            <div class="row">
+                <div class="col-sm-2 unpeuplace">
+                    <input type="reset" id="reset" value="Annuler" />
+                </div>
+                <div class="col-sm-4 unpeuplace">
+                    <input type="submit" name="commander" id="inscrire" value="Finaliser mon inscription" />         
+                </div>
+            </div>
+        </form>
     </div>
+</div>
